d9ad138c33875d27eb204d29b7a8c04d1dfe6708
norahbobo427-ai <norahbobo427@gmail.com>
2025-09-19 16:16:49 +0300

    Add files via upload

---
+++ All units program.cpp
@@ -0,0 +1,20 @@
+/*
+Name:Norah Bancy Wambui
+Reg:CT100/G/26156/25
+Description:All units program
+*/
+
+#include<stdio.h>
+int main() 
+
+{
+	printf("Analogue electronics\n");
+	printf("Computer systems and organization\n");
+	printf("Fundamentals of ICT\n");
+	printf("Introduction to computer applications\n");
+	printf("Essentials of accounting\n");
+	printf("Intoduction to programming and algorithims\n");
+	printf("Mathematics for science\n");
+	printf("Communication skills\n");
+}
+	
