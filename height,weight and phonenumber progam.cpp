c8836b705fb079818ec491a760d593845e26f6d7
norahbobo427-ai <norahbobo427@gmail.com>
2025-09-19 16:18:29 +0300

    Add files via upload

---
+++ height,weight and phonenumber progam.cpp
@@ -0,0 +1,28 @@
+/*
+Name: Norah Bancy Wambui Gachoki
+Reg:CT100/G/26156/25
+Description:Height,phonenumber and weight program
+*/
+#include<stdio.h>
+
+int main(){
+    int height;
+    int phonenumber;
+    int weight;
+    
+    printf("\nEnter your height:");
+    scanf("%d",&height);
+    
+    printf("\nEnter your phonenumber:");
+    scanf("%d",&phonenumber);
+    
+    printf("\nEnter your weight:");
+    scanf("%d",&weight);
+    
+    printf("Height is:%d m\n",height);
+    printf("The phonenumber is:%d\n",phonenumber);
+    printf("Weight:%d kg\n",weight);
+	
+	return 0;
+}
+	
